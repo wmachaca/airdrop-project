d449cd0317bb81f1d4ff59f22e5bb2e11012b94f
William I <william.machaca@hotmail.com>
2025-06-11 15:16:49 -0300

    feature/contracts/Improve RatherToken tests/LIN

--- script/DeployRatherToken.s.sol
+++ script/DeployRatherToken.s.sol
@@ -5,10 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {RatherToken} from "../contracts/RatherToken.sol";
 
 contract DeployRatherToken is Script {
-    function run(uint256 initialSupply) external returns(RatherToken) {
+    function run(uint256 initialSupply) external returns (RatherToken) {
         vm.startBroadcast();
         RatherToken rt = new RatherToken(initialSupply);
         vm.stopBroadcast();
-        return rt; 
+        return rt;
     }
 }
--- test/RatherToken.t.sol
+++ test/RatherToken.t.sol
@@ -4,18 +4,17 @@ pragma solidity ^0.8.16;
 import {Test} from "forge-std/Test.sol";
 import {DeployRatherToken} from "../script/DeployRatherToken.s.sol";
 import {RatherToken} from "../contracts/RatherToken.sol";
+
 contract RatherTokenTest is Test {
     RatherToken public ratherToken;
     DeployRatherToken public deployer;
 
     //Events like in IERC20.sol: only for testing
-    event Transfer(address indexed from, address indexed to, uint256 value);    
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     //Error
     error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
-    
-    
-    
+
     // Test addresses
     //address owner = makeAddr("owner");-> msg.sender
     address user1 = makeAddr("user1");
@@ -27,33 +26,28 @@ contract RatherTokenTest is Test {
     uint256 constant TRANSFER_AMOUNT = 100 ether;
     uint256 constant TRANSFER_AMOUNTE = 500 ether;
     uint256 constant APPROVAL_AMOUNT = 300 ether;
-    uint256 constant TEST_MINT_AMOUNT = 500 ether;    
+    uint256 constant TEST_MINT_AMOUNT = 500 ether;
 
     function setUp() public {
         //Deploy
         deployer = new DeployRatherToken();
         ratherToken = deployer.run(INITIAL_SUPPLY);
 
-        //msg.sender is test contract
-        //vm.startPrank(address(deployer));
-        //ratherToken.transferOwnership(owner);
-        //vm.stopPrank();
-
         //Fund test users from owner
-        vm.prank(msg.sender);//mock
-        ratherToken.transfer(user1,TRANSFER_AMOUNT);
+        vm.prank(msg.sender); //mock
+        ratherToken.transfer(user1, TRANSFER_AMOUNT);
     }
 
     // Deployment
     function testDeploymentInitialSupply() public view {
         assertEq(ratherToken.totalSupply(), INITIAL_SUPPLY);
-        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY-TRANSFER_AMOUNT);
+        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY - TRANSFER_AMOUNT);
         assertEq(ratherToken.balanceOf(user1), TRANSFER_AMOUNT);
     }
 
     function testDeploymentOwnerIsInitialOwner() public view {
         assertEq(ratherToken.owner(), msg.sender);
-    }    
+    }
 
     function testTokenMetadataCorrect() public view {
         assertEq(ratherToken.name(), "RatherToken");
@@ -65,7 +59,7 @@ contract RatherTokenTest is Test {
     function testTransferSuccess() public {
         vm.prank(user1);
         ratherToken.transfer(user2, TRANSFER_AMOUNT / 2);
-        
+
         assertEq(ratherToken.balanceOf(user1), TRANSFER_AMOUNT / 2);
         assertEq(ratherToken.balanceOf(user2), TRANSFER_AMOUNT / 2);
     }
@@ -79,26 +73,21 @@ contract RatherTokenTest is Test {
 
     function testTransferFailsIfInsufficientBalance() public {
         vm.prank(user1);
-        vm.expectRevert(abi.encodeWithSelector(
-                ERC20InsufficientBalance.selector,
-                user1,
-                100 ether,
-                500 ether
-            ));
+        vm.expectRevert(abi.encodeWithSelector(ERC20InsufficientBalance.selector, user1, 100 ether, 500 ether));
         ratherToken.transfer(user2, TRANSFER_AMOUNTE);
-    }    
+    }
 
     function testTransferFromSuccess() public {
         // Owner approves spender
         vm.prank(msg.sender);
         ratherToken.approve(spender, APPROVAL_AMOUNT);
-        
+
         // Spender transfers from owner to user1
         vm.prank(spender);
         ratherToken.transferFrom(msg.sender, user1, TRANSFER_AMOUNT);
-        
-        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY - 2*TRANSFER_AMOUNT);
-        assertEq(ratherToken.balanceOf(user1), 2*TRANSFER_AMOUNT);
+
+        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY - 2 * TRANSFER_AMOUNT);
+        assertEq(ratherToken.balanceOf(user1), 2 * TRANSFER_AMOUNT);
         assertEq(ratherToken.allowance(msg.sender, spender), APPROVAL_AMOUNT - TRANSFER_AMOUNT);
     }
 
@@ -106,7 +95,7 @@ contract RatherTokenTest is Test {
     function testApproveSuccess() public {
         vm.prank(msg.sender);
         ratherToken.approve(spender, APPROVAL_AMOUNT);
-        
+
         assertEq(ratherToken.allowance(msg.sender, spender), APPROVAL_AMOUNT);
     }
 
@@ -114,12 +103,10 @@ contract RatherTokenTest is Test {
     function testTransferZeroAmount() public {
         vm.prank(msg.sender);
         ratherToken.transfer(user1, 0);
-        
-        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY-TRANSFER_AMOUNT);
+
+        assertEq(ratherToken.balanceOf(msg.sender), INITIAL_SUPPLY - TRANSFER_AMOUNT);
         assertEq(ratherToken.balanceOf(user1), TRANSFER_AMOUNT);
     }
 
-
-    // Ownership 
-
+    // Ownership
 }
